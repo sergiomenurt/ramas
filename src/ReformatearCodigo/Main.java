c6030653cedf3ea2a17079bcc74daa09622969bd
Sergio Mendez Urtaza <smendezurtaza@danielcastelao.org>
2022-01-21 08:58:29 +0100

    Initial commit

---
+++ src/ReformatearCodigo/Main.java
@@ -0,0 +1,14 @@
+package ReformatearCodigo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //A
+        //B
+        //C
+        //1
+        //2
+    }
+}
